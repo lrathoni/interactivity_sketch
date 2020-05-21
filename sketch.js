7bdc79679ec0910b23d63ddd96262a7445716893
Line <line.jpn25@gmail.com>
2020-05-21 17:51:43 +0200

    character add and move

--- sketch.js
+++ sketch.js
@@ -1,39 +1,49 @@
 let img;
-
-function preload() {
-    img = loadImage('perso.png');
-}
+let stairs;
+let buttonUp;
+let buttonDown;
+let characterArray = [];
+// function preload() {
+//     img = loadImage('https://github.com/lrathoni/interactivity_sketch/blob/master/perso.png');
+// }
 
 function setup() {
     createCanvas(1500, 800);
     stairs = new Stairs();
-    manager = new ManageCharacter(stairs);
+    //characterArray.push(new Character( stairs.steps[0].characterArrayXPosition[0] , 540, 0));
+    console.log(characterArray);
+    buttonUp = createButton('+');
+    buttonUp.position(100, 540);
+    buttonUp.mousePressed(createCharacter);
+
+    buttonDown = createButton('-');
+    buttonDown.position(100, 600);
+    buttonDown.mousePressed(deleteCharacter);
   }
   
   function draw() {
     background(220);
-
-    // fill(150)
-    // rect(500,600, 400, 60);
-    manager.myStairs.drawStairs();
-
-    manager.createCharacter();
-
+    stairs.drawStairs();
+    if (characterArray.length > 0)
+        drawCharacterArray();
   }
+
+
 
 
   //Step Class
 class Step {
-    static num = 6;
+    static num = 5;
     constructor(x_position, y_position, w_width, h_height) {
         this.number =  
         this.width = w_width;
         this.height = h_height;
         this.x = x_position;
         this.y = y_position;
-        this.characterArrayPosition = [];
+        this.characterArrayXPosition = [];
+        this.characterArrayYPosition = [];
         for (let i=0; i<6 ; i++) {
-                this.characterArrayPosition.push(i * (this.width/5) + (this.width/5)/2 );
+                this.characterArrayXPosition.push(this.x + i * (this.width/5) + this.width/35 );
         }
     }
 
@@ -47,9 +57,9 @@ class Step {
 class Stairs {
     constructor() {
         this.steps = [];
-        this.steps.push(new Step(600,600, 600, 60))
-        for (let i = 1; i < 5; i ++) {
-            this.steps.push(new Step( this.steps[0].x - (this.steps[i-1].width*0.9 - this.steps[0].x )/2 , this.steps[i-1].y - this.steps[i-1].height*0.9, this.steps[i-1].width*0.9 , this.steps[i-1].height*0.9 ));
+        this.steps.push(new Step(200,600, 1000, 60))
+        for (let i = 1; i < 6; i ++) {
+            this.steps.push(new Step( this.steps[i-1].x + (this.steps[i-1].width*0.05) , this.steps[i-1].y - this.steps[i-1].height*0.9, this.steps[i-1].width*0.9 , this.steps[i-1].height*0.9 ));
         }
     }
 
@@ -59,73 +69,100 @@ class Stairs {
 }
 
   //character Class
-class character {
-    static NbCharacter = [0,0,0,0,0] ;
-
+class Character {
     constructor(x_position, y_position, k) {
         this.numberCharacter = k;
         switch(this.numberCharacter) {
             case 0:
                 this.keyUp = 'e';
                 this.KeyDown = 'd';
+                break;
             case 1:
                 this.keyUp = 'r';
                 this.KeyDown = 'f';
+                break;
             case 2:
                 this.keyUp = 't';
                 this.KeyDown = 'g';
+                break;
             case 3:
                 this.keyUp = 'o';
                 this.KeyDown = 'l';
+                break;
             case 4:
                 this.keyUp = 'p';
                 this.KeyDown = 'm';
+                break;
+        }
         this.x = x_position;
         this.y = y_position;
-        this.imgPerso = image(img, this.x, this.y)
+        //this.imgPerso = image(img, this.x, this.y)
+        //this.imgPerso = rect(this.x, this.y, 80, 200);
         this.stepPosition = 0 ;
         }
+
+    drawCharacter() {
+        fill(240)
+        rect(this.x, this.y, 80, 200);
     }
 
-    movekeyPressed(StepArrayX) {
-        if (Key == this.up) {
-            if (this.stepPosition < 6)
-                this.stepPosition+=1;
-                this.x = newXPosition;
-                this.y += 60;
-                this.imgPerso = image(img, this.x, this.y);
-        }
+    moveUp(stairs) {
+            if (this.stepPosition < 5) {
+                this.stepPosition += 1;
+                this.x = stairs.steps[this.stepPosition].characterArrayXPosition[this.numberCharacter];
+                this.y -= stairs.steps[this.stepPosition-1].height;
+            }
+    }
+    moveDown(stepDown) {
+            if (this.stepPosition > 0) {
+                this.stepPosition -= 1;
+                this.x = stairs.steps[this.stepPosition].characterArrayXPosition[this.numberCharacter];
+                this.y += stairs.steps[this.stepPosition].height;
+            }
+    }
+
+    movekeyPressed(key, newPos) {
+        if (key == this.keyUp)
+            this.moveUp(newPos)
+        if (key == this.KeyDown)
+            this.moveDown(newPos)
     }
 
 }
 
-class ManageCharacter {
-    constructor(stairs) {
-        this.myStairs = stairs;
-        this.characterArrayNumber = [0,0,0,0,0];
-        this.characterArray = [null, null, null, null, null];
-    }
 
-    createCharacter() {
-        let k = 0;
-        while (k<5) {
-            if (this.characterArray[k] == null) {
-                this.characterArrayNumber[k] == k+1;
-                this.characterArray[k] = new character( this.myStairs.steps[0].characterArrayPosition[k] , 540, k);
-                return;
-            }
-            k++;
+function createCharacter() {
+    console.log(characterArray)
+        if (characterArray.length <5) {
+            console.log("k =" + characterArray.length )
+            const k = characterArray.length;
+            characterArray.push(new Character( stairs.steps[0].characterArrayXPosition[k] , 460, k));
+            console.log("ici" + characterArray[k]);
         }
-    }
+}
 
-    deleteCharacter() {
-        let k = 0;
-        while (k<5) {
-            if (this.characterArray[k] != null) {
-                delete this.characterArray[k];
-                return;
-            }
-            k++
+function deleteCharacter() {
+        if (characterArray.length > 0) {
+            const k = characterArray.length;
+            //this.charactercharacterArrayNumber[k] == -1; 
+            characterArray.pop();
         }
-    }
+}
+
+function drawCharacterArray() {
+    //console.log(array);
+    characterArray.forEach(element => element.drawCharacter());
+}
+
+function keyPressed() {
+    if( (key == 'e' || key == 'd') && characterArray.length >= 1)
+        characterArray[0].movekeyPressed(key, stairs)
+    if( (key == 'r' || key == 'f') && characterArray.length >= 2)
+        characterArray[1].movekeyPressed(key, stairs)
+    if( (key == 't' || key == 'g') && characterArray.length >= 3)
+        characterArray[2].movekeyPressed(key, stairs)
+    if( (key == 'o' || key == 'l') && characterArray.length >= 4)
+        characterArray[3].movekeyPressed(key, stairs)
+    if( (key == 'p' || key == 'm') && characterArray.length >= 5)
+        characterArray[4].movekeyPressed(key, stairs)
 }
